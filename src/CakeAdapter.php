b5d90c06d25443672ae1ec2f1e592002ca1ce85f
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2023-09-13 18:01:07 +0200

    #633 Quote schema on postgres

--- src/CakeAdapter.php
+++ src/CakeAdapter.php
@@ -58,7 +58,7 @@ class CakeAdapter extends AdapterWrapper
         if ($connection->getDriver() instanceof Postgres) {
             $config = $connection->config();
             $schema = empty($config['schema']) ? 'public' : $config['schema'];
-            $pdo->exec('SET search_path TO ' . $schema);
+            $pdo->exec('SET search_path TO ' . $connection->getDriver()->quoteIdentifier($schema));
         }
         $connection->getDriver()->setConnection($pdo);
     }
